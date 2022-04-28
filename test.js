cd1b7353860aa32c72275efa5d1bdebd43ec71bf
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-28 14:48:56 -0300

    fixed about text on mobile, testing title

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-// find the div I want to show
 const about = document.querySelector('.about')
 const inner_text = document.querySelector('.about__inner-text')
-const knowhow = document.querySelector('.knowhow')
-// listen for scroll to show
-// after intro__text translateX(0)
+const intro_text = document.querySelector('.intro__text')
 
 function obCallback(payload) {
   if(payload[0].intersectionRatio === 1) {
     inner_text.classList.remove("outside")
+    intro_text.classList.toggle("send-to-back")
   }
 }
 
@@ -16,4 +14,5 @@ const ob = new IntersectionObserver(obCallback, {
   // threshold: 1,
 });
 
-ob.observe(about.lastElementChild)
+ob.observe(about.firstElementChild)
+console.log(intro_text)
